fe15612ced801ce9e8a281d169eeffd768d1967c
Per Andre <perandre@gmail.com>
2010-02-19 10:12:17 +0100

    module update, frontman theme added

--- sites/all/modules/tinymce_node_picker/tinymce-node-picker-page.tpl.php
+++ sites/all/modules/tinymce_node_picker/tinymce-node-picker-page.tpl.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: tinymce-node-picker-page.tpl.php,v 1.5 2010/01/15 08:31:58 blixxxa Exp $
+// $Id: tinymce-node-picker-page.tpl.php,v 1.6 2010/02/17 08:19:15 blixxxa Exp $
 /**
  * @file
  * Theme template to display a TinyMCE Node Picker dialog page.
@@ -14,9 +14,9 @@
   <?php print $scripts; ?>
   <script type="text/javascript"><?php /* Needed to avoid Flash of Unstyled Content in IE */ ?> </script>
 </head>
-<body>
-<div id="nodepicker">
-<?php if (!empty($tabs)): ?><div class="clear-block"><ul class="tabs primary"><?php print $tabs; ?></ul></div><?php endif; ?>
+<body id="nodepicker">
+<div class="nodepicker">
+<?php if (!empty($tabs)): ?><div class="clear-block"><?php print $tabs; ?></div><?php endif; ?>
 <?php if (!empty($messages)): print $messages; endif; ?>
 <?php if (!empty($help)): print $help; endif; ?>
 <div class="clear-block">
